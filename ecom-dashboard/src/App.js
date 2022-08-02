eee6ef53b625e07e3ff913ff45af098b570eca43
bijoymandal <bijoymandal00@gmail.com>
2022-08-02 07:24:13 +0530

    react set up project

---
+++ ecom-dashboard/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>bootstrap</h1>  
+      <button>Normal Button</button>
+      <Button variant="success">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
